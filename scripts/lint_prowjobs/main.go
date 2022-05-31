a103e169b653d007d206ba66f5cd47b1df66b9ab
jonahjon <jonahjones094@gmail.com>
2022-05-31 17:09:13 -0500

    adding prow jobs for generatebundlefile
    Signed-off-by: jonahjon <jonahjones094@gmail.com>

--- scripts/lint_prowjobs/main.go
+++ scripts/lint_prowjobs/main.go
@@ -151,7 +151,8 @@ func MakeTargetCheck(jc *JobConstants) presubmitCheck {
 			presubmitConfig.JobBase.Name == "eks-anywhere-cluster-controller-tooling-presubmit" ||
 			presubmitConfig.JobBase.Name == "eks-anywhere-release-tooling-presubmit" ||
 			presubmitConfig.JobBase.Name == "eks-anywhere-release-tooling-test-presubmit" ||
-			presubmitConfig.JobBase.Name == "eks-anywhere-packages-presubmit" {
+			presubmitConfig.JobBase.Name == "eks-anywhere-packages-presubmit" ||
+			presubmitConfig.JobBase.Name == "eks-anywhere-packages-generatebundle-presubmit" {
 			return true, 0, ""
 		}
 		jobMakeTargetMatches := regexp.MustCompile(`make (\w+[-\w]+?)(?: -C \S+)?`).FindAllStringSubmatch(strings.Join(presubmitConfig.JobBase.Spec.Containers[0].Command, " "), -1)
