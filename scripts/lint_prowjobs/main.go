22cb26ca76d1a0bc4c7d9521080fdc5ba44849da
Jackson West <jgw@amazon.com>
2022-11-10 08:37:09 -0600

    Adds source-controller arm presubmit

--- scripts/lint_prowjobs/main.go
+++ scripts/lint_prowjobs/main.go
@@ -123,6 +123,10 @@ func BucketCheck(jc *JobConstants) presubmitCheck {
 
 func ClusterCheck(jc *JobConstants) presubmitCheck {
 	return presubmitCheck(func(presubmitConfig config.Presubmit, fileContentsString string) (bool, int, string) {
+		if strings.Contains(presubmitConfig.JobBase.Name, "arm64") {
+			return true, 0, ""
+		}
+
 		if presubmitConfig.JobBase.Cluster != jc.Cluster {
 			return false, findLineNumber(fileContentsString, "cluster:"), fmt.Sprintf(`Incorrect cluster configuration, please configure cluster as => cluster: "%s"`, jc.Cluster)
 		}
@@ -133,6 +137,9 @@ func ClusterCheck(jc *JobConstants) presubmitCheck {
 func ServiceAccountCheck(jc *JobConstants) presubmitCheck {
 	return presubmitCheck(func(presubmitConfig config.Presubmit, fileContentsString string) (bool, int, string) {
 		if strings.Contains(presubmitConfig.JobBase.Name, "e2e") {
+			return true, 0, ""
+		}
+		if strings.Contains(presubmitConfig.JobBase.Name, "arm64") {
 			return true, 0, ""
 		}
 		if presubmitConfig.JobBase.Spec.ServiceAccountName != jc.ServiceAccountName {
